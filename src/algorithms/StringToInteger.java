f701e68a926bc5b0ed9997ab0adb9a3f0f4d4a46
freefly0706 <1741093834@qq.com>
2017-08-23 14:58:00 +0800

    Modify String to Integer algorithm

--- src/algorithms/StringToInteger.java
+++ src/algorithms/StringToInteger.java
@@ -3,8 +3,7 @@ package algorithms;
 
 public class StringToInteger {
     public int myAtoi(String str) {
-        StringBuilder resultStr = new StringBuilder();
-        int result;
+        long result = 0;
         int sign = 1;
         int i = 0;
         for (; i < str.length(); i++) {
@@ -20,18 +19,12 @@ public class StringToInteger {
             i++;
         }
         while (i < str.length() && '0' <= str.charAt(i) && str.charAt(i) <= '9') {
-            resultStr.append(str.charAt(i++));
+            int digit = str.charAt(i++) - '0';
+            result = result * 10 + digit;
+            if (result > Integer.MAX_VALUE) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
         }
-        if (resultStr.length() > 10 || (resultStr.length() == 10
-                && (Integer.parseInt(resultStr.substring(0, 9)) > Integer.MAX_VALUE / 10 ||
-                (Integer.parseInt(resultStr.substring(0, 9)) == Integer.MAX_VALUE / 10 &&
-                        (resultStr.charAt(9) - '0' > Integer.MAX_VALUE % 10))))) {
-            return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-        }
-        if (resultStr.length() == 0) {
-            return 0;
-        }
-        result = Integer.parseInt(resultStr.toString());
-        return result * sign;
+        return (int) result * sign;
     }
 }
