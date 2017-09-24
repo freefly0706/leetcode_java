f35edaeed4cb2b963751346b51cf1962de53f18a
freefly0706 <1741093834@qq.com>
2017-09-24 20:31:10 +0800

    Add Container With Most Water algorithm

---
+++ src/algorithms/ContainerWithMostWater.java
@@ -0,0 +1,16 @@
+/* -*- coding: UTF-8 -*- */
+package algorithms;
+
+public class ContainerWithMostWater {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int max = 0, minBound;
+        while (left < right) {
+            minBound = Math.min(height[left], height[right]);
+            max = Math.max(max, minBound * (right - left));
+            while (left < height.length && height[left] <= minBound) left++;
+            while (right > 0 && height[right] <= minBound) right--;
+        }
+        return max;
+    }
+}
