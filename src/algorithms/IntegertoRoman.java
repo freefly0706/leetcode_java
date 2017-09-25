502ec867842476b6ec81a93f0dd3dbf944def86b
freefly0706 <1741093834@qq.com>
2017-09-25 20:10:31 +0800

    Add Integer to Roman algorithm

---
+++ src/algorithms/IntegertoRoman.java
@@ -0,0 +1,19 @@
+/* -*- coding: UTF-8 -*- */
+package algorithms;
+
+public class IntegertoRoman {
+    public String intToRoman(int num) {
+        StringBuilder res = new StringBuilder();
+        String[] baseString = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] baseInt = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        for (int i = 0; i < baseString.length; i++) {
+            while (num >= baseInt[i]) {
+                if (num >= baseInt[i]) {
+                    res.append(baseString[i]);
+                    num -= baseInt[i];
+                }
+            }
+        }
+        return res.toString();
+    }
+}
