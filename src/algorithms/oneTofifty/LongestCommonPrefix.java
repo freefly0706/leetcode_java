f1cb3e2272ac055cad64f58f08e4c78cc41a3632
freefly0706 <1741093834@qq.com>
2017-09-28 15:42:39 +0800

    Add Longest Common Prefix algorithm

---
+++ src/algorithms/oneTofifty/LongestCommonPrefix.java
@@ -0,0 +1,29 @@
+/* -*- coding: UTF-8 -*- */
+package algorithms.oneTofifty;
+
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        StringBuilder res = new StringBuilder();
+        if (strs.length == 0) {
+            return "";
+        }
+        int shortestStrIndex = 0;
+        int shortestLen = strs[0].length();
+        for (int i = 1; i < strs.length; i++) {
+            if (strs[i].length() < shortestLen) {
+                shortestStrIndex = i;
+                shortestLen = strs[i].length();
+            }
+        }
+        String shortestStr = strs[shortestStrIndex];
+        for (int i = 0; i < shortestLen; i++) {
+            for (int j = 0; j < strs.length; j++) {
+                if (shortestStr.charAt(i) != strs[j].charAt(i)) {
+                    return res.toString();
+                }
+            }
+            res.append(shortestStr.charAt(i));
+        }
+        return res.toString();
+    }
+}
