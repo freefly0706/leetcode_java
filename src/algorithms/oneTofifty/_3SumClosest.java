82c166ba1f9307e826b16485e30abcd1f2ad1e7a
freefly0706 <1741093834@qq.com>
2017-10-14 23:10:52 +0800

    Add 3Sum Closest algorithm

---
+++ src/algorithms/oneTofifty/_3SumClosest.java
@@ -0,0 +1,40 @@
+/* -*- coding: UTF-8 -*- */
+package algorithms.oneTofifty;
+
+import java.util.Arrays;
+
+public class _3SumClosest {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int length = nums.length;
+        int highBound = length;
+        int closestMin = nums[0] + nums[1] + nums[2];
+        int closestMax = nums[length - 3] + nums[length - 2] + nums[length - 1];
+        for (int i = 0; i < length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int low = i + 1, high = highBound - 1;
+            while (low < high) {
+                int sum = nums[i] + nums[low] + nums[high];
+                if (sum == target) {
+                    return target;
+                } else if (sum < target) {
+                    closestMin = Math.max(closestMin, sum);
+                    while (low < high && nums[low] == nums[low + 1]) low++;
+                    low++;
+                } else {
+                    if (low == i + 1) {  //如果当前最小值加上当前最大值比target大,则当前最大值不必再参加后续计算
+                        highBound--;
+                    }
+                    closestMax = Math.min(closestMax, sum);
+                    while (low < high && nums[high] == nums[high - 1]) high--;
+                    high--;
+                }
+            }
+        }
+        if ((target - closestMin) > (closestMax - target)) {
+            return closestMax;
+        } else {
+            return closestMin;
+        }
+    }
+}
