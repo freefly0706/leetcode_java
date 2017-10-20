baf87bab53cdfb4b65db4d5cd95a5666c43b4bcb
freefly0706 <1741093834@qq.com>
2017-10-20 15:31:14 +0800

    Modify 3Sum algorithm

--- src/algorithms/one2fifty/_3Sum.java
+++ src/algorithms/one2fifty/_3Sum.java
@@ -9,22 +9,27 @@ public class _3Sum {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res = new LinkedList<>();
         Arrays.sort(nums);
+        int highBound = nums.length;
         int positiveLowBound = 0;
-        while (positiveLowBound < nums.length && nums[positiveLowBound] <= 0) positiveLowBound++;
+        while (positiveLowBound < highBound && nums[positiveLowBound] <= 0) positiveLowBound++;
 
         for (int i = 0; i < positiveLowBound; i++) {
             if (i > 0 && (nums[i] == nums[i - 1])) continue;
-            int low = i + 1, high = nums.length - 1;
+            int low = i + 1, high = highBound - 1,sum;
             while (low < high) {
-                if (nums[i] + nums[low] + nums[high] == 0) {
+                sum = nums[i] + nums[low] + nums[high];
+                if (sum == 0) {
                     res.add(Arrays.asList(nums[i], nums[low], nums[high]));
                     while (low < high && nums[low + 1] == nums[low]) low++;
                     while (low < high && nums[high - 1] == nums[high]) high--;
                     low++;
                     high--;
-                } else if (nums[i] + nums[low] + nums[high] < 0) {
+                } else if (sum < 0) {
                     low++;
                 } else {
+                    if (low == i + 1) {
+                        highBound--;
+                    }
                     high--;
                 }
             }
