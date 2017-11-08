b1042e5cf28e39984e393ce0c733e90cb4407da8
freefly0706 <1741093834@qq.com>
2017-11-08 09:17:25 +0800

    Modify Letter Combinations of a Phone Number algorithm

--- src/algorithms/one2fifty/LetterCombinationsOfAPhoneNumber.java
+++ src/algorithms/one2fifty/LetterCombinationsOfAPhoneNumber.java
@@ -7,22 +7,25 @@ import java.util.List;
 public class LetterCombinationsOfAPhoneNumber {
     public List<String> letterCombinations(String digits) {
         List<String> res = new LinkedList<>();
-        if (digits.length() == 0) {
+        int digitsLength = digits.length();
+        if (digitsLength == 0) {
             return res;
         }
-        String[] pairs = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        backTracking("", pairs, 0, digits, res);
+        String[] digitCorString = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        backTracking("", digitCorString, 0, digits, digitsLength, res);
         return res;
     }
 
-    private void backTracking(String combination, String[] pairs, int index, String digits, List<String> res) {
-        if (index == digits.length()) {
+    private void backTracking(String combination, String[] digitCorString, int index, String digits,
+                              int digitsLength, List<String> res) {
+        if (index == digitsLength) {
             res.add(combination);
             return;
         }
         int char2int = digits.charAt(index) - '0';
-        for (int i = 0; i < pairs[char2int].length(); i++) {
-            backTracking(combination + pairs[char2int].charAt(i), pairs, index + 1, digits, res);
+        for (int i = 0; i < digitCorString[char2int].length(); i++) {
+            backTracking(combination + digitCorString[char2int].charAt(i), digitCorString,
+                    index + 1, digits, digitsLength, res);
         }
     }
 }
