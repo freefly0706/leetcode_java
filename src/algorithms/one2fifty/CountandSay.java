1da05656ecb045964e8d4d922f80defbe99b262a
e <1741093834@qq.com>
2017-12-06 21:12:27 +0800

    Add Count and Say algorithm

--- src/algorithms/one2fifty/CountandSay.java
+++ src/algorithms/one2fifty/CountandSay.java
@@ -4,21 +4,20 @@ public class CountandSay {
     public String countAndSay(int n) {
         if (n == 1) {
             return "1";
-        } else {
-            String previous = countAndSay(n - 1);
-            StringBuilder res = new StringBuilder();
-            int count = 1;
-            int length = previous.length();
-            for (int i = 1; i < length; i++) {
-                if (previous.charAt(i - 1) == previous.charAt(i)) {
-                    count++;
-                } else {
-                    res.append(count).append(previous.charAt(i - 1));
-                    count = 1;
-                }
-            }
-            res.append(count).append(previous.charAt(length - 1));
-            return res.toString();
         }
+        String previous = countAndSay(n - 1);
+        StringBuilder res = new StringBuilder();
+        int count = 1;
+        int length = previous.length();
+        for (int i = 1; i < length; i++) {
+            if (previous.charAt(i - 1) == previous.charAt(i)) {
+                count++;
+            } else {
+                res.append(count).append(previous.charAt(i - 1));
+                count = 1;
+            }
+        }
+        res.append(count).append(previous.charAt(length - 1));
+        return res.toString();
     }
 }
