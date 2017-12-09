418fcfea5c2f5ee511d11536910b844e13bc0c62
e <1741093834@qq.com>
2017-12-09 09:54:08 +0800

    Add Combination Sum II algorithm

---
+++ src/algorithms/one2fifty/CombinationSumII.java
@@ -0,0 +1,35 @@
+package algorithms.one2fifty;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CombinationSumII {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        int candidatesNum = candidates.length;
+        List<List<Integer>> res = new ArrayList<>();
+        backTracking(res, new LinkedList<>(), 0, candidatesNum, candidates, target);
+        return res;
+    }
+
+    private void backTracking(List<List<Integer>> res, LinkedList<Integer> combination, int start, int candidatesNum,
+                              int[] candidates, int target) {
+        if (target == 0) {
+            res.add(new ArrayList<>(combination));
+            return;
+        }
+        if (target < 0) {
+            return;
+        }
+        for (int i = start; i < candidatesNum; i++) {
+            if (i > start && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+            combination.add(candidates[i]);
+            backTracking(res, combination, i + 1, candidatesNum, candidates, target - candidates[i]);
+            combination.removeLast();
+        }
+    }
+}
