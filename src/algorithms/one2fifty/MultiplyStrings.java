78e80b8d7b7914fc4daa3c2bf9b3d6c56b84951e
e <1741093834@qq.com>
2017-12-22 12:14:05 +0800

    Add Multiply Strings algorithm

---
+++ src/algorithms/one2fifty/MultiplyStrings.java
@@ -0,0 +1,27 @@
+package algorithms.one2fifty;
+
+public class MultiplyStrings {
+    public String multiply(String num1, String num2) {
+        if (num1.charAt(0) == '0' || num2.charAt(0) == '0') {
+            return "0";
+        }
+        int num1Len = num1.length(), num2Len = num2.length();
+        int[] resInt = new int[num1Len + num2Len];
+        for (int i = num1Len - 1, k = 0; i >= 0; k++, i--) {
+            for (int j = num2Len - 1, m = 0; j >= 0; m++, j--) {
+                int tmp = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                int p1 = i + j, p2 = p1 + 1;
+                tmp += resInt[p2];
+                resInt[p2] = tmp % 10;
+                resInt[p1] += tmp / 10;
+            }
+        }
+        StringBuilder resStr = new StringBuilder();
+        int i = -1;
+        while (resInt[++i] == 0) ;
+        for (; i < resInt.length; i++) {
+            resStr.append(resInt[i]);
+        }
+        return resStr.toString();
+    }
+}
