537ae8e75a94b48378cd5271d98b9ac9240340a2
e <1741093834@qq.com>
2018-01-06 10:23:30 +0800

    Add Rotate Image algorithm

--- src/algorithms/one2fifty/RotateImage.java
+++ src/algorithms/one2fifty/RotateImage.java
@@ -1,6 +1,23 @@
 package algorithms.one2fifty;
 
 public class RotateImage {
+    /*把图片分为行数/2层，然后一层层进行旋转，
+    每一层有上下左右四个列，目标就是把上列放到右列，
+    右列放到下列，下列放到左列，左列放回上列*/
+    public void rotate(int[][] matrix) {
+        int dim = matrix.length;
+        for (int i = 0; i < dim / 2; i++) {
+            for (int j = i; j < dim - 1 - i; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[dim - 1 - j][i];
+                matrix[dim - 1 - j][i] = matrix[dim - 1 - i][dim - 1 - j];
+                matrix[dim - 1 - i][dim - 1 - j] = matrix[j][dim - 1 - i];
+                matrix[j][dim - 1 - i] = tmp;
+            }
+        }
+    }
+
+    /*
     public void rotate(int[][] matrix) {
         int dim = matrix.length;
         for (int i = 1; i < dim; i++) {
@@ -19,4 +36,5 @@ public class RotateImage {
             }
         }
     }
+    */
 }
