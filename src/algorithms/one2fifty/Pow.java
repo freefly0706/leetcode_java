d6388e1928fa037a41e38a7ecd1c8e1f6f651348
e <1741093834@qq.com>
2018-02-24 11:47:05 +0800

    Add

---
+++ src/algorithms/one2fifty/Pow.java
@@ -0,0 +1,26 @@
+package algorithms.one2fifty;
+
+public class Pow {
+    public double myPow(double x, int n) {
+        if (n > 0) {
+            n = -n;
+        } else {
+            x = 1 / x;
+        }
+        return binaryPow(x, n);
+    }
+
+    public double binaryPow(double x, int n) {
+        if (n == 0) {
+            return 1.0;
+        }
+        if (n == -1) {
+            return x;
+        }
+        double left = binaryPow(x, n / 2);
+        if (n % 2 == 0) {
+            return left * left;
+        }
+        return left * left * x;
+    }
+}
